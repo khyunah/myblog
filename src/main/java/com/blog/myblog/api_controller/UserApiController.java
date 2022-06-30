10606a91555426ac9de257aac4ac6c08ceddca3b
khyunah <89136556+khyunah@users.noreply.github.com>
2022-06-30 23:05:07 +0900

    회원가입 완료 ( ajax 통신 처리 )

--- src/main/java/com/blog/myblog/api_controller/UserApiController.java
+++ src/main/java/com/blog/myblog/api_controller/UserApiController.java
@@ -3,6 +3,7 @@ package com.blog.myblog.api_controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blog.myblog.dto.ResponseDto;
@@ -17,7 +18,7 @@ public class UserApiController {
 
 	// 회원가입 
 	@PostMapping("/auth/user/join")
-	public ResponseDto<User> joinUser(User user) {
+	public ResponseDto<User> joinUser(@RequestBody User user) {
 		return new ResponseDto<>(HttpStatus.OK.value(), userService.joinUser(user));  
 	}
 }
--- src/main/webapp/WEB-INF/views/layout/header.jsp
+++ src/main/webapp/WEB-INF/views/layout/header.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,7 +10,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
 
 <!-- jQuery library -->
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 <!-- Popper JS -->
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
